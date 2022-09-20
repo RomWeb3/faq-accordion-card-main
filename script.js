3c73564e25362149805f5f180cd247d76377d236
RomWeb3 <mspulco07@gmail.com>
2022-09-20 16:46:12 +0200

    Factorised my JS script

--- script.js
+++ script.js
@@ -3,22 +3,10 @@ const content = document.querySelectorAll('.content');
 
 
 
-for (i=0; i < content.length; i++) {
+for (let i=0; i < content.length; i++) {
 
-    content[0].addEventListener('click', () => {
-        content[0].classList.toggle('active');
-    }); 
-    content[1].addEventListener('click', () => {
-        content[1].classList.toggle('active');
-    }); 
-    content[2].addEventListener('click', () => {
-        content[2].classList.toggle('active');
-    }); 
-    content[3].addEventListener('click', () => {
-        content[3].classList.toggle('active');
-    }); 
-    content[4].addEventListener('click', () => {
-        content[4].classList.toggle('active');
+    content[i].addEventListener('click', () => {
+        content[i].classList.toggle('active');
     }); 
 }
 
